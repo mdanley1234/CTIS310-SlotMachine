879de6c81fb3e50a647e7360738e790d8e8c1529
mdanley1234 <112212314+mdanley1234@users.noreply.github.com>
2025-01-09 15:39:37 -0500

    Gambling Machine is Done

--- slot-machine/src/main/java/edu/guilford/Main.java
+++ slot-machine/src/main/java/edu/guilford/Main.java
@@ -1,10 +1,61 @@
 package edu.guilford;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        // Call the analyzeMachines() method to analyze the slot machines
+        analyzeMachines();
+        System.out.println("\n\n\n");
+
         System.out.println("Let's gamble!");
 
         SlotMachine slotMachine = new SlotMachine();
-        slotMachine.play();
+
+        // When user hits the enter key, the program will run the slotMachine.play() method
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("You have " + slotMachine.getStart_amount() + " dollars");
+            System.out.println("It costs " + slotMachine.getCost() + " dollars to play");
+            System.out.println("You can win " + slotMachine.getWin() + " dollars");
+            System.out.println("Press enter to play or type 'exit' to quit");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            slotMachine.play();
+        }
+    }
+
+    public static void analyzeMachines() {
+        // Instantiate one object with values for all attributes
+        SlotMachine firstMachine = new SlotMachine(3, 3, 10, 50);
+
+        // Create an ArrayList to store SlotMachine objects
+        ArrayList<SlotMachine> slotMachines = new ArrayList<>();
+
+        // Add the first object to the ArrayList
+        slotMachines.add(firstMachine);
+
+        // Loop to instantiate at least five objects and add them to the ArrayList
+        for (int i = 0; i < 5; i++) {
+            SlotMachine machine = new SlotMachine();
+            slotMachines.add(machine);
+        }
+
+        // Loop to print out all elements of the ArrayList
+        for (SlotMachine machine : slotMachines) {
+            System.out.println(machine);
+        }
+
+        // Analysis of the objects
+        int totalWinAmount = 0;
+        for (SlotMachine machine : slotMachines) {
+            totalWinAmount += machine.getWin();
+        }
+        double averageWinAmount = totalWinAmount / (double) slotMachines.size();
+        System.out.println("Average win amount of all slot machines: " + averageWinAmount);
     }
 }
--- slot-machine/src/main/java/edu/guilford/SlotMachine.java
+++ slot-machine/src/main/java/edu/guilford/SlotMachine.java
@@ -1,5 +1,6 @@
 package edu.guilford;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SlotMachine {
@@ -9,6 +10,24 @@ public class SlotMachine {
     // Attributes
     private int wheels = 3; // Number of wheels
     private int symbols = 5; // Number of symbols
+    private int start_amount = 100; // Starting amount of money
+    private int cost = 10; // Bet cost
+    private int win = 1000; // Win amount
+
+    // Constructors
+    public SlotMachine(int wheels, int symbols, int cost, int win) {
+        this.wheels = wheels;
+        this.symbols = symbols;
+        this.cost = cost;
+        this.win = win;
+    }
+
+    public SlotMachine() {
+        this.wheels = random.nextInt(10) + 1;
+        this.symbols = random.nextInt(10) + 1;
+        this.cost = random.nextInt(46) + 5;
+        this.win = random.nextInt(901) + 100;
+    }
 
     enum Fruit {
         SEVEN,
@@ -50,19 +69,99 @@ public class SlotMachine {
         return Fruit.values()[pick];
     }
 
-    // Play method
+    @Override
+    public String toString() {
+        return "SlotMachine{" +
+                "wheels=" + wheels +
+                ", symbols=" + symbols +
+                ", start_amount=" + start_amount +
+                ", cost=" + cost +
+                ", win=" + win +
+                '}';
+    }
+
+    // Helper method | The play method invokes the spin method to account for each wheel
+    // If the user wins the method adds money to the start_amount and if the user loses the method subtracts money from the start_amount
     public void play() {
-        Fruit slot1 = spin();
-        Fruit slot2 = spin();
-        Fruit slot3 = spin();
 
-        System.out.println(slot1 + " " + slot2 + " " + slot3);
-
-        if (slot1 == slot2 && slot2 == slot3) {
-            System.out.println("You win!");
-        } else {
-            System.out.println("You lose!");
+        if (start_amount < cost) {
+            System.out.println("You don't have enough money to play!");
+            return;
         }
+
+        ArrayList<Fruit> fruits = new ArrayList<Fruit>();
+
+        System.out.println("---------------------------");
+
+        for (int i = 0; i < wheels; i++) {
+            Fruit fruit = spin();
+            fruits.add(fruit);
+            System.out.print(fruit + " ");
+        }
+
+        System.out.println("\n---------------------------");
+
+        // Check if all fruits in the fruits array list are the same
+        boolean allSame = true;
+        for (Fruit fruit : fruits) {
+            if (fruit != fruits.get(0)) {
+            allSame = false;
+            break;
+            }
+        }
+        System.out.println();
+
+        if (allSame) {
+            System.out.println("YOU WIN!");
+            start_amount += win;
+        } else {
+            System.out.println("YOU LOSE!");
+            start_amount -= cost;
+        }
+        
+    }
+
+
+    // Getters and Setters
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public int getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(int symbols) {
+        this.symbols = symbols;
+    }
+
+
+    public int getStart_amount() {
+        return start_amount;
+    }
+
+    public void setStart_amount(int start_amount) {
+        this.start_amount = start_amount;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
     }
 
 }
